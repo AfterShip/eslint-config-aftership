69e5b1d8cca14eae13ec9fc8a09ec4ea529b5662
Alex Yeung <foray1010@gmail.com>
2016-01-06 11:50:34 +0800

    Add jsx-no-bind(error) and react/no-is-mounted(warning)

--- react.js
+++ react.js
@@ -12,11 +12,21 @@ module.exports = extendConfig({
 		// https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent-props.md
 		'react/jsx-indent-props': [2, 'tab'],
 
-		// we want to define multiple components within one file
+		// Will move to `error` when `isMounted` is deprecated
+		// https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-is-mounted.md
+		'react/no-is-mounted': 1,
+
+		// We want to define multiple components within one file
 		// for example, for table.jsx, define header component and footer component in the same file
+		// https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-multi-comp.md
 		'react/no-multi-comp': 0,
 
-		// don't want to check default props
+		// ES6 class is not so stable using babel.js and it requires developers to define variables out of the class
+		// https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-es6-class.md
+		'react/prefer-es6-class': 0,
+
+		// Don't want to check default props
+		// https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
 		'react/prop-types': [2, {ignore: ['children']}]
 	}
 });
